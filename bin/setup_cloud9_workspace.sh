d6afca4582a5dcd41aeb2e720cfccd5d511e253a
Daniel Boline <daniel.boline@stonybrook.edu>
2014-11-27 09:16:33 -0500

    install xml2 when using cloud9

--- bin/setup_cloud9_workspace.sh
+++ bin/setup_cloud9_workspace.sh
@@ -4,6 +4,6 @@ sudo echo
 SCRIPTDIR="${HOME}/workspace"
 
 sudo apt-get update
-sudo apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap
+sudo apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap xml2
 
 # python garmin.py build
