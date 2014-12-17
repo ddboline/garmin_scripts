f833d224aef43318bbf7367a15d6fca4928b8048
Daniel Boline <daniel.boline@stonybrook.edu>
2014-12-17 08:39:39 -0500

    ...

--- bin/setup_terminal_workspace.sh
+++ bin/setup_terminal_workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap xml2
+apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap xml2 python-lockfile
 
 # python garmin.py build
