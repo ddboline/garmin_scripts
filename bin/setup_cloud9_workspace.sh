412004d4411053e046390b31e1fe9359e1233f4f
Daniel Boline <ddboline@gmail.com>
2015-01-01 20:02:26 -0500

    ...

--- bin/setup_cloud9_workspace.sh
+++ bin/setup_cloud9_workspace.sh
@@ -6,3 +6,13 @@ SCRIPTDIR="${HOME}/workspace"
 sudo apt-get update
 sudo apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap xml2
 sudo apt-get install -y python-pandas
+
+echo "America/New_York" > timezone
+sudo mv timezone /etc/timezone
+sudo rm /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/posixrules /etc/localtime
+
+./garmin.py get
+./garmin.py year run
+python unittests.py
+./running_pace_plot.py 
