d6b40a75367d7579106ca66991940f0f7848ca30
Daniel Boline <ddboline@gmail.com>
2015-01-02 00:40:36 +0000

    ...

--- bin/setup_cloud9_workspace.sh
+++ bin/setup_cloud9_workspace.sh
@@ -5,5 +5,4 @@ SCRIPTDIR="${HOME}/workspace"
 
 sudo apt-get update
 sudo apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap xml2
-
-# python garmin.py build
+sudo apt-get install -y python-pandas
