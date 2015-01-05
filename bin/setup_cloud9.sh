8fbcc720679094c6441811fd33c043f8672cfa0e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-01-05 14:48:49 -0500

    ...

--- bin/setup_cloud9.sh
+++ bin/setup_cloud9.sh
@@ -5,7 +5,7 @@ SCRIPTDIR="${HOME}/workspace"
 
 sudo apt-get update
 sudo apt-get install -y gpsbabel garmin-forerunner-tools python-mpltoolkits.basemap xml2
-sudo apt-get install -y python-pandas
+sudo apt-get install -y python-pandas python-lockfile
 
 echo "America/New_York" > timezone
 sudo mv timezone /etc/timezone
