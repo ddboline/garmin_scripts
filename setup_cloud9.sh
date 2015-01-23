8c54a207dc81913dd98323324ee32dcbff638896
Daniel Boline <ddboline@gmail.com>
2015-01-23 10:10:22 -0500

    use CLOUDVERSION branch for ddboline_html in cloud

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y python-pandas python-lockfile
 
 CURDIR=`pwd`
 cd ${HOME}
-git clone https://github.com/ddboline/ddboline_html.git public_html
+git clone -b CLOUDVERSION https://github.com/ddboline/ddboline_html.git public_html
 cd public_html/
 sh setup_cloud9.sh
 mkdir garmin
@@ -16,6 +16,6 @@ cd $CURDIR
 echo "America/New_York" > timezone
 sudo mv timezone /etc/timezone
 sudo rm /etc/localtime
-sudo ln -sf /usr/share/zoneinfo/posixrules /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 
 ./test_cloud9.sh
