e0eaa225039a06edf6d896cfe7cf99b12b0ecde7
Daniel Boline <ddboline@gmail.com>
2015-01-25 16:14:02 -0500

    make html component of setup optional

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -5,17 +5,16 @@ sudo apt-get install -y gpsbabel garmin-forerunner-tools xml2
 # sudo apt-get install -y python-mpltoolkits.basemap
 sudo apt-get install -y python-pandas python-lockfile
 
-CURDIR=`pwd`
-cd ${HOME}
-git clone -b CLOUDVERSION https://github.com/ddboline/ddboline_html.git public_html
-cd public_html/
-sh setup_cloud9.sh
-mkdir garmin
-cd $CURDIR
-
-# echo "America/New_York" > timezone
-# sudo mv timezone /etc/timezone
-# sudo rm /etc/localtime
-# sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+if [ -z $1 ] ; then
+    true
+elif [ $1 == "html" ] ; then
+    CURDIR=`pwd`
+    cd ${HOME}
+    git clone -b CLOUDVERSION https://github.com/ddboline/ddboline_html.git public_html
+    cd public_html/
+    sh setup_cloud9.sh
+    mkdir garmin
+    cd $CURDIR
+fi
 
 ./test_cloud9.sh
