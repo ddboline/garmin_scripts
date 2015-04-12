5b0163fbfd29ce04fd482e94d6ee8fde1193a39c
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-12 19:21:45 -0400

    pylint

--- running_pace_regression.py
+++ running_pace_regression.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+""" Fit my race results to linear model """
 from __future__ import print_function
 
 import os
@@ -12,81 +13,79 @@ import math
 
 HOMEDIR = os.getenv('HOME')
 
-meters_per_mile = 1609.344 # meters
-km_per_mile = meters_per_mile / 1000
-marathon_distance_m = 42195 # meters
-marathon_distance_mi = 42195 / meters_per_mile # meters
+METERS_PER_MILE = 1609.344 # meters
+KM_PER_MILE = METERS_PER_MILE / 1000
+MARATHON_DISTANCE_M = 42195 # meters
+MARATHON_DISTANCE_MI = 42195 / METERS_PER_MILE # meters
 
-class running_pace:
-    def __init__(self, d=0.0, pmpm=0.0, y=2014, f=True, name=''):
-        self.distance = d
+
+class RunningPace(object):
+    """ Class to contain running pace entry """
+    def __init__(self, dist=0.0, pmpm=0.0, year=2014, flag=True, name=''):
+        self.distance = dist
         self.pace = pmpm
-        self.year = y
-        self.flag = f
+        self.year = year
+        self.flag = flag
         self.name = name
 
     def read_line(self, line):
+        """ read line in running_paces.txt file """
         ents = line.split()
         self.distance = float(ents[0])
-        m = int(ents[1])
-        s = int(ents[2])
-        self.pace = float(m) + float(s) / 60.
+        minute = int(ents[1])
+        second = int(ents[2])
+        self.pace = float(minute) + float(second) / 60.
         self.year = int(ents[3])
         self.flag = int(ents[4])
         self.name = ents[5]
 
+
 def read_pace_file(fname='%s/scripts/running_paces.txt' % HOMEDIR):
+    """ read running paces file """
     running_paces = []
 
     for line in open(fname, 'r').xreadlines():
-        r = running_pace()
-        r.read_line(line)
-        running_paces.append(r)
+        rp_ = RunningPace()
+        rp_.read_line(line)
+        running_paces.append(rp_)
 
     return running_paces
 
-# def func(x, a = 1):
-    # return a * x**0.06 / 26.2**1.06
-
-# def func(x):
-    # return x
-
 def running_pace_regression():
+    """ run regression """
     running_paces = read_pace_file()
 
     random.shuffle(running_paces)
 
-    p = []
-    d = []
-    for r in running_paces:
-        p.append(math.log(r.pace))
-        d.append(math.log(r.distance))
+    paces = []
+    dists = []
+    for rp_ in running_paces:
+        paces.append(math.log(rp_.pace))
+        dists.append(math.log(rp_.distance))
 
-    p = np.array(p)
-    d = np.array(d)
+    paces = np.array(paces)
+    dists = np.array(dists)
 
-    p_train = p[0:50]
-    d_train = d[0:50, np.newaxis]
-    p_test = p[-50:]
-    d_test = d[-50:, np.newaxis]
+    p_train = paces[0:50]
+    d_train = dists[0:50, np.newaxis]
+    p_test = paces[-50:]
+    d_test = dists[-50:, np.newaxis]
 
 
-    p_train = p
-    d_train = d[:, np.newaxis]
-    p_test = p
-    d_test = d[:, np.newaxis]
+    p_train = paces
+    d_train = dists[:, np.newaxis]
+    p_test = paces
+    d_test = dists[:, np.newaxis]
 
     regr = linear_model.LinearRegression()
     regr.fit(d_train, p_train)
 
-    # print(dir(regr))
     print(regr.coef_)
 
     print(np.mean((regr.predict(d_test) - p_test)**2))
 
     plt.scatter(d_test, p_test, color='black')
-    plt.plot(d_test, regr.predict(d_test), color='blue',
-            linewidth=3)
+    plt.plot(d_test, regr.predict(d_test), color='blue', linewidth=3)
 
     plt.xticks(())
     plt.yticks(())
