ee3d8c5f56049f57c65c0bbcefdb047996175436
Daniel Boline <ddboline@gmail.com>
2015-11-27 09:08:42 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python-numpy python-matplotlib python-scipy python-nose python-sklearn python-coverage 
+sudo apt-get install -y python-numpy python-matplotlib python-scipy python-nose \
+                        python-sklearn python-coverage python-setuptools python-dev
