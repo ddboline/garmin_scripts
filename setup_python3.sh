8d6322c9a2968ce696331d8bfd002995ea5e9ca8
Daniel Boline <ddboline@gmail.com>
2015-11-28 12:07:08 -0500

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -5,5 +5,5 @@ export LANG="C.UTF-8"
 
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y --force-yes python3-numpy=1.\* python3-matplotlib \
+sudo apt-get install -y --force-yes python3-numpy=1.\* python3-matplotlib python3-setuptools \
                                     python3-scipy python3-nose python3-sklearn
