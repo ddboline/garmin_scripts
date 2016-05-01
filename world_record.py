ba971e315ac023fe260e1f9b990b5f43b762ac30
Daniel Boline <ddboline@gmail.com>
2016-05-01 17:26:05 -0400

    ...

--- world_record.py
+++ world_record.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 """ fit world record paces to simple model """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 import os
 import numpy as np
 import matplotlib
