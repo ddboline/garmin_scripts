37609682a4c5f921619269a50665732a9d2e8a29
Daniel Boline <ddboline@gmail.com>
2016-08-24 06:06:13 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -3,4 +3,5 @@
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python-numpy=1.\* python-matplotlib python-scipy python-nose \
-                                    python-sklearn python-coverage python-setuptools python-dev
+                                    python-sklearn python-coverage python-setuptools python-dev \
+                                    python-pandas
