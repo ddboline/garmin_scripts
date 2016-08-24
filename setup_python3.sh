82b3eed90160659cf3b6249314fd7cbaee379d25
Daniel Boline <ddboline@gmail.com>
2016-08-24 05:45:20 -0400

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -3,7 +3,7 @@
 ### hack...
 export LANG="C.UTF-8"
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python3-numpy=1.\* python3-matplotlib python3-setuptools \
                                     python3-scipy python3-nose python3-sklearn
