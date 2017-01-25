7dc13166bb15dbeadc9de8349f5b8a7e7f4bfcfe
Daniel Boline <ddboline@gmail.com>
2017-01-25 15:48:24 -0500

    add prediction based on average marathon finish, cleanup a bit

--- running_pace_plot.py
+++ running_pace_plot.py
@@ -73,11 +73,14 @@ def plot_paces(fname):
     print('best marathon pace', print_m_s(pbest * 60))
     print('')
 
-    def pow_func(xval, *params):
-        """ ... """
-        x0_ = MARATHON_DISTANCE_M/METERS_PER_MILE
-        return param0 * (xval/x0_)**params[0]
+    def pow_func_gen(param=param0):
+        def pow_func(xval, *params):
+            """ ... """
+            x0_ = MARATHON_DISTANCE_M/METERS_PER_MILE
+            return param * (xval/x0_)**params[0]
+        return pow_func
 
+    pow_func = pow_func_gen(param0)
     xval = np.linspace(1, MARATHON_DISTANCE_MI, 100)
     rp_low = rp_[rp_[:, 0] < MARATHON_DISTANCE_MI]
     rp_high = rp_[rp_[:, 0] >= MARATHON_DISTANCE_MI]
@@ -100,10 +103,14 @@ def plot_paces(fname):
     print('p', params, '+/-', dparams)
     print('')
 
-    def pow_func_best(xval, *params):
-        """ ... """
-        x0_ = MARATHON_DISTANCE_M/METERS_PER_MILE
-        return pbest * (xval/x0_)**params[0]
+    p50k = pow_func(50e3/METERS_PER_MILE, 0.28938393) * 60
+    p50m = pow_func(50, 0.28938393) * 60
+    print('average 50k estimate', print_m_s(p50k),
+          print_h_m_s(p50k * 50e3/METERS_PER_MILE))
+    print('average 50mi estimate', print_m_s(p50m), print_h_m_s(p50m * 50))
+    print('')
+
+    pow_func_best = pow_func_gen(pbest)
 
     p50k = pow_func_best(50e3/METERS_PER_MILE, 0.28938393) * 60
     p50m = pow_func_best(50, 0.28938393) * 60
@@ -114,10 +121,7 @@ def plot_paces(fname):
 
     p315 = (3 * 60 + 15) / (MARATHON_DISTANCE_M/METERS_PER_MILE)
 
-    def pow_func_315(xval, *params):
-        """ ... """
-        x0_ = MARATHON_DISTANCE_M/METERS_PER_MILE
-        return p315 * (xval/x0_)**params[0]
+    pow_func_315 = pow_func_gen(p315)
 
     p50k = pow_func_315(50e3/METERS_PER_MILE, 0.28938393) * 60
     p50m = pow_func_315(50, 0.28938393) * 60
@@ -130,10 +134,7 @@ def plot_paces(fname):
 
     p300 = (3 * 60) / (MARATHON_DISTANCE_M / METERS_PER_MILE)
 
-    def pow_func_300(xval, *params):
-        """ ... """
-        x0_ = MARATHON_DISTANCE_M/METERS_PER_MILE
-        return p300 * (xval/x0_)**params[0]
+    pow_func_300 = pow_func_gen(p300)
 
     p50k = pow_func_300(50e3/METERS_PER_MILE, 0.28938393) * 60
     p50m = pow_func_300(50, 0.28938393) * 60
