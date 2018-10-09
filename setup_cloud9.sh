ca491ef42214339251730cdda75373dffdbbaa19
Daniel Boline <ddboline@gmail.com>
2018-10-09 05:56:18 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo bash -c "echo deb ssh://ddboline@home.ddboline.net/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python-numpy=1.\* python-matplotlib python-scipy python-pytest \
                                     python-sklearn python-coverage python-setuptools python-dev \
--- setup_python3.sh
+++ setup_python3.sh
@@ -3,7 +3,7 @@
 ### hack...
 export LANG="C.UTF-8"
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo bash -c "echo deb ssh://ddboline@home.ddboline.net/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes python3-numpy=1.\* python3-matplotlib python3-setuptools \
                                     python3-scipy python3-pytest python3-pytest-cov \
