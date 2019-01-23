0759c469f1fb0befd76142bf2c3677c7344a465d
Daniel Boline <ddboline@gmail.com>
2019-01-22 22:21:08 -0500

    move auth code to separate flask api

--- extract_heartrate_data.py
+++ extract_heartrate_data.py
@@ -2,16 +2,17 @@
 import cherrypy
 import os
 import sys
-import threading
+
 import traceback
 import webbrowser
 import datetime
 import requests
 import pandas as pd
 from dateutil.parser import parse
+from urllib.parse import urlencode
 
 from pytz import timezone
-from time import strftime
+from time import strftime, sleep
 
 from base64 import b64encode
 import fitbit
@@ -31,87 +32,37 @@ est = timezone(strftime("%Z").replace('CST', 'CST6CDT').replace('EDT', 'EST5EDT'
 client_id = '228D9P'
 client_secret = '9d7aa34320fac07106dca853dab8603d'
 
-
-class OAuth2Server:
-
-    def __init__(self, client_id, client_secret, redirect_uri='http://127.0.0.1:8080/'):
-        """ Initialize the FitbitOauth2Client """
-        self.success_html = """
-            <h1>You are now authorized to access the Fitbit API!</h1>
-            <br/><h3>You can close this window</h3>"""
-        self.failure_html = """
-            <h1>ERROR: %s</h1><br/><h3>You can close this window</h3>%s"""
-
-        self.fitbit = fitbit.Fitbit(
-            client_id,
-            client_secret,
-            redirect_uri=redirect_uri,
-            timeout=10,
-        )
-
-    def browser_authorize(self):
-        """
-        Open a browser to the authorization url and spool up a CherryPy
-        server to accept the response
-        """
-        url, _ = self.fitbit.client.authorize_token_url()
-        # Open the web browser in a new thread for command-line browser support
-        threading.Timer(1, webbrowser.open, args=(url, )).start()
-        cherrypy.quickstart(self)
-
-    @cherrypy.expose
-    def index(self, state, code=None, error=None):
-        """
-        Receive a Fitbit response containing a verification code. Use the code
-        to fetch the access_token.
-        """
-        error = None
-        if code:
-            try:
-                self.fitbit.client.fetch_access_token(code)
-            except MissingTokenError:
-                error = self._fmt_failure('Missing access token parameter.</br>Please check that '
-                                          'you are using the correct client_secret')
-            except MismatchingStateError:
-                error = self._fmt_failure('CSRF Warning! Mismatching state')
-        else:
-            error = self._fmt_failure('Unknown error while authenticating')
-        # Use a thread to shutdown cherrypy so we can return HTML first
-        self._shutdown_cherrypy()
-        return error if error else self.success_html
-
-    def _fmt_failure(self, message):
-        tb = traceback.format_tb(sys.exc_info()[2])
-        tb_html = '<pre>%s</pre>' % ('\n'.join(tb)) if tb else ''
-        return self.failure_html % (message, tb_html)
-
-    def _shutdown_cherrypy(self):
-        """ Shutdown cherrypy in one second, if it's running """
-        if cherrypy.engine.state == cherrypy.engine.states.STARTED:
-            threading.Timer(1, cherrypy.engine.exit).start()
+current_token = {}
 
 
 def get_client(refresh=False):
     if refresh:
-        server = OAuth2Server(client_id, client_secret)
-        server.browser_authorize()
-        access_token = server.fitbit.client.session.token['access_token']
-        refresh_token = server.fitbit.client.session.token['refresh_token']
-        user_id = server.fitbit.client.session.token['user_id']
-        with open('%s/.fitbit_tokens' % os.getenv('HOME'), 'w') as fd:
-            fd.write('user_id=%s\n' % user_id)
-            fd.write('access_token=%s\n' % access_token)
-            fd.write('refresh_token=%s\n' % refresh_token)
+        url = 'https://www.ddboline.net/fitbit/auth?%s' % urlencode({
+            'id': client_id,
+            'secret': client_secret
+        })
+        webbrowser.open(requests.get(url).text)
+        sleep(5)
+
+    with open('%s/.fitbit_tokens' % os.getenv('HOME'), 'r') as fd:
+        for line in fd:
+            key, val = line.strip().split('=')
+            if key == 'user_id':
+                user_id = val
+            elif key == 'access_token':
+                access_token = val
+            elif key == 'refresh_token':
+                refresh_token = val
+
+    if (user_id == current_token.get('user_id', '') and
+            access_token == current_token.get('access_token', '') and
+            refresh_token == current_token.get('refresh_token', '')):
+        input('Something didnt work')
     else:
-        with open('%s/.fitbit_tokens' % os.getenv('HOME'), 'r') as fd:
-            for line in fd:
-                key, val = line.strip().split('=')
-                if key == 'user_id':
-                    user_id = val
-                elif key == 'access_token':
-                    access_token = val
-                elif key == 'refresh_token':
-                    refresh_token = val
+        current_token['user_id'] = user_id
+        current_token['access_token'] = access_token
+        current_token['refresh_token'] = refresh_token
+
     client = fitbit.Fitbit(
         client_id, client_secret, access_token=access_token, refresh_token=refresh_token)
     try:
@@ -145,6 +96,7 @@ def get_heartrate_data(begin_date='2017-03-10', end_date=datetime.date.today().i
         js = requests.get(f'https://www.ddboline.net/garmin/list_gps_tracks?filter={date}').json()
         files.extend(js['gps_list'])
     for fname in files:
+        print(fname)
         js = requests.get(f'https://www.ddboline.net/garmin/get_hr_data?filter={fname}').json()
         tmp = [{
             'time': parse(x['time']).astimezone(est).isoformat()[:19],
